9edbf07c10459dca80e5b7367ba33ced44dc0e95
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-08-22 13:41:14 -0300

    Modificado arquivo persistence.xml

--- src/main/java/br/com/testecdi/domain/Pessoa.java
+++ src/main/java/br/com/testecdi/domain/Pessoa.java
@@ -1,5 +1,23 @@
 package br.com.testecdi.domain;
 
-public class Pessoa {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Pessoa extends AbstractEntity {
+
+	private static final long serialVersionUID = 3860885131403764281L;
+
+	@NotEmpty(message = "O preenchimento do campo nome é obrigatório.")
+	@Size(min = 5, max = 50, message = "Tamanho inválido para o campo nome (5 - 50")
+	@Column(name = "nome", nullable = false)
+	private String nome;
 
 }
