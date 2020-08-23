b114582ee7354292ec889b8447b75e8d2f715352
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-08-23 09:16:15 -0300

    Finalizado o metodo salvar no Bean da classe Pessoa

--- src/main/java/br/com/testecdi/bean/PessoaBean.java
+++ src/main/java/br/com/testecdi/bean/PessoaBean.java
@@ -3,12 +3,27 @@ package br.com.testecdi.bean;
 import java.io.Serializable;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import org.omnifaces.util.Messages;
+
+import br.com.testecdi.domain.Pessoa;
+import br.com.testecdi.service.PessoaService;
 
 @Named
 @RequestScoped
 public class PessoaBean implements Serializable {
 
 	private static final long serialVersionUID = -7520922911940543207L;
+
+	@Inject
+	private PessoaService pessoaService;
+
+	public void salvar(Pessoa pessoa) {
+		pessoaService.salvar(pessoa);
+
+		Messages.addGlobalInfo("Pessoa salva com sucesso.");
+	}
 
 }
