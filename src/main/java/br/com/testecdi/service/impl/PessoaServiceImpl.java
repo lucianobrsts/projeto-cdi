67808e597230bfbbad66f6de9f23c1b36a10929e
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-08-23 09:10:34 -0300

    Criado o pacote serviceImpl do projeto

---
+++ src/main/java/br/com/testecdi/service/impl/PessoaServiceImpl.java
@@ -0,0 +1,8 @@
+package br.com.testecdi.service.impl;
+
+import br.com.testecdi.domain.Pessoa;
+import br.com.testecdi.service.PessoaService;
+
+public class PessoaServiceImpl extends GenericServiceImpl<Pessoa> implements PessoaService {
+
+}
