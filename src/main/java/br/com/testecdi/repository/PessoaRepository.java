d81b6fdb4b672e6af56392272ecc4687638d0f78
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-08-24 14:52:13 -0300

    Modificada estrutura do projeto

--- src/main/java/br/com/testecdi/repository/PessoaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.testecdi.repository;
-
-import br.com.testecdi.domain.Pessoa;
-
-public interface PessoaRepository extends JpaGenericRepository<Pessoa> {
-
-}
--- src/main/java/br/com/testecdi/service/impl/PessoaServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package br.com.testecdi.service.impl;
-
-import br.com.testecdi.domain.Pessoa;
-import br.com.testecdi.service.PessoaService;
-
-public class PessoaServiceImpl extends GenericServiceImpl<Pessoa> implements PessoaService {
-
-}
